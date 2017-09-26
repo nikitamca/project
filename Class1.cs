ecdbe57eb927cc009f317986305780fe8296a1e6
nikitamca <parmarnikita119@gmail.com>
2017-09-26 21:34:53 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace OMS.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int eid { get; set; }
+
+        [Required]
+        public string ename { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string cpassword { get; set; }
+        public int mono { get; set; }
+    }
+}
---
+++ Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OMS.Models
+{
+    public class Context:DbContext
+    {
+        public Class1 cls { get; set; }
+        public Context() : base(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Admin\Desktop\Office-Management-System-master\OMS\App_Data\Database1.mdf;Integrated Security=True") { }
+    }
+}
